d74ab515b77d23934c63e24901b255e584313c66
catying <chenmai1102@gmail.com>
2017-12-01 14:24:43 +0800

    Half

---
+++ admin/weibo_config/apps.py
@@ -0,0 +1,5 @@
+from django.apps import AppConfig
+
+
+class WeiboConfigConfig(AppConfig):
+    name = 'weibo_config'
---
+++ admin/weibo_data/apps.py
@@ -0,0 +1,5 @@
+from django.apps import AppConfig
+
+
+class WeiboDataConfig(AppConfig):
+    name = 'weibo_data'
